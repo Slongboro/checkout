f6b5ecb6af5a3201952e4e4203342dc21d6e65e5
slongboro <susanlongley@gmail.com>
2019-10-22 10:00:45 +0100

    test that checkout responds to item

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,7 +1,14 @@
+require 'checkout'
 
 describe Checkout do
 
-  it 'should create to a new instance of checkout'
+  it 'should create to a new instance of checkout' do
   checkout = Checkout.new
   expect(checkout).to be_kind_of(Checkout)
+end
+
+it 'checkout should respond to item' do
+  checkout = Checkout.new
+  expect(checkout).to respond_to(item)
+end
 end
